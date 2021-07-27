18afaf835faad334853824bedaea0a01e69b22e1
Maria Clara Visconti <claravisconti@hotmail.com>
2021-07-27 18:23:51 -0300

    nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,38 @@
 import React from 'react';
 import '../App.css';
 import { Navbar, Container, NavDropdown, Nav } from 'react-bootstrap';
-
+import logo from '../logo.svg';
 
 function NavBar() {
     return (
         <div>
             {/* Nabvar */}
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar collapseOnSelect expand="lg" className="pinkNav">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="#home">Bloom</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            <Nav.Link href="#features">Inicio</Nav.Link>
+                            <NavDropdown title="Colección" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Camperas</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Camisas</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Blusas</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.1">Pantalones</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Vestidos</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Zapatos</NavDropdown.Item>
                             </NavDropdown>
+                            <Nav.Link href="#pricing">Sale</Nav.Link>
+                            <Nav.Link href="#pricing">Contacto</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                                Dank memes
-                            </Nav.Link>
+
+                            <NavDropdown title="Clara Visconti" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Mi cuenta</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Carrito</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Cerrar Sesión</NavDropdown.Item>
+                            </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
