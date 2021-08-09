299853e36ef53b392fba86a8fd353908d0363ca4
Maria Clara Visconti <claravisconti@hotmail.com>
2021-08-09 19:13:07 -0300

    boton

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -7,32 +7,18 @@ class ItemCount extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = ({
-            item: 1
-        });
-    }
-
-    remove(item) {
-        if (item >= 1) {
-            this.setState(item = item - 1)
-            console.log("remove");
-        }
-    }
-
-    add(item) {
-        if (item >= 1) {
-            this.setState(item = item + 1)
-            console.log("add");
-        }
+        this.state = {
+            count: 1
+        };
     }
 
     render() {
 
         return (
             <div>
-                <p>Cantidad:{this.state.item}</p>
-                <button type="button" onClick={this.remove.bind(this)}>-</button>
-                <button type="button" onClick={this.add.bind(this)}>+</button>
+                <p>Cantidad:{this.state.count}</p>
+                <button onClick={() => this.setState({ count: this.state.count - 1 })}>-</button>
+                <button onClick={() => this.setState({ count: this.state.count + 1 })}>+</button>
                 <button type="button">Agregar al carrito</button>
             </div>
         );
