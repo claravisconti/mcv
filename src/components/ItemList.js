ccdbabf25a16964528e78bc64c28b75a44275d18
Maria Clara Visconti <claravisconti@hotmail.com>
2021-08-10 13:20:05 -0300

    commit

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import '../App.css';
+import { Container, Row, Col } from 'react-bootstrap';
 import Item from './Item';
+import vestidoClara from '../images/vestidoClara.jpg';
+import vestidoSofia from '../images/vestidoSofia.jpg';
+import vestidoLuna from '../images/vestidoLuna.jpg';
+
+const dresses = [
+    {
+        id: 1,
+        title: "Vestido Clara",
+        price: "$5000",
+        image: vestidoClara,
+    },
+    {
+        id: 2,
+        title: "Vestido Sofia",
+        price: "$5000",
+        image: vestidoSofia,
+    },
+    {
+        id: 3,
+        title: "Vestido Luna",
+        price: "$5000",
+        image: vestidoLuna,
+    }
+];
 
 function ItemList() {
     return (
         <div>
-             <Item/>
+            <Container>
+                <Row>
+                <Col>
+                    {
+                        dresses.map(item => {
+                            return <Item id={item.id} title={item.title} price={item.price} image={item.image} />
+                        })
+                    }
+                    </Col>
+                </Row>
+            </Container>
+
         </div>
     );
 }
