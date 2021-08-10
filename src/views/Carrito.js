9c7dd3f7086953c30d43964796740bb5283585dd
Maria Clara Visconti <claravisconti@hotmail.com>
2021-08-10 18:14:35 -0300

    secciones

---
+++ src/views/Carrito.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../App.css";
+import { Container, Row, Col, Nav } from "react-bootstrap";
+import ItemCount from '../components/ItemCount';
+
+function Carrito() {
+    return (
+        <div>
+            <Container>
+                <Row>
+        
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default Carrito;
