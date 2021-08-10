fe7c85ae137c6dd0cbd265baa18f6a0e361491f3
Maria Clara Visconti <claravisconti@hotmail.com>
2021-08-10 13:06:45 -0300

    fixes

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import '../App.css';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
-function Item() {
+const Item = ({ id, title, price, image }) => {
     return (
         <div>
+            <Card>
+                <CardImg top width="100%" src={image} alt={title} />
+                <CardBody>
+                    <CardTitle tag="h5">{title}</CardTitle>
+                    <CardText>{price}</CardText>
+                </CardBody>
+            </Card>
         </div>
     );
-}
+};
 
 export default Item;
