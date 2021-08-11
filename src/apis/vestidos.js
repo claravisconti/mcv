54e7957dbaf1941269ad0f2b6f7393db2d9c0f51
Maria Clara Visconti <claravisconti@hotmail.com>
2021-08-10 21:39:23 -0300

    json

--- src/apis/vestidos.js
+++ src/apis/vestidos.js
@@ -6,20 +6,33 @@ const vestidos = [
     {
         id: 1,
         title: "Vestido Clara",
-        price: "$5000",
+        price: "$8.990",
+        descripcion:"Vestido confeccionado en flamécon silueta linea a, y frunces tipo volados en cuerpo y mangas, escote redondo y mangas acampanadas.",
         image: vestidoClara,
+        colores: ['rojo','amarillo','blanco'],
+        talles: [40,42,44,46],
+        fotos: [vestidoClara,vestidoClara,vestidoClara]
+        
     },
     {
         id: 2,
         title: "Vestido Sofia",
-        price: "$5000",
+        price: "$7.490",
+        descripcion:"Camisa larga confeccionada en heavy voile, look chemise. con mangas balloon 3/4 y recorte en 2da cadera con pequeños frunces.",
         image: vestidoSofia,
+        colores: ['rojo','amarillo','blanco'],
+        talles: [40,42,44,46],
+        fotos: [vestidoClara,vestidoClara,vestidoClara]
     },
     {
         id: 3,
         title: "Vestido Luna",
-        price: "$5000",
+        price: "$5.800",
+        descripcion:"Camisa larga confeccionada en heavy voile, look chemise. con mangas balloon 3/4 y recorte en 2da cadera con pequeños frunces.",
         image: vestidoLuna,
+        colores: ['rojo','amarillo','blanco'],
+        talles: [40,42,44,46],
+        fotos: [vestidoClara,vestidoClara,vestidoClara]
     }
 ]
 
