75422a37ba49cde1eab59d1cc24509a693cff7fd
Maria Clara Visconti <claravisconti@hotmail.com>
2021-08-19 10:55:58 -0300

    menu

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,6 +1,7 @@
 // Components
 import React from 'react';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { Navbar, Container, NavDropdown, Nav } from 'react-bootstrap';
 import Home from '../views/Home';
 import Ingreso from '../views/Ingreso';
 import Registro from '../views/Registro';
@@ -18,7 +19,44 @@ function Router() {
 
         <BrowserRouter>
 
-            <div>
+            <Navbar collapseOnSelect expand="lg" className="pinkNav">
+                <Container>
+                    <Navbar.Brand href="#home"><h3 className="logo">Bloom</h3></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#"><Link to="/">Inicio</Link></a>
+                            </li>
+                            <NavDropdown title="Colección" id="collasible-nav-dropdown">
+                                <li><a class="dropdown-item" href="#"><Link to="/productos">Abrigos</Link></a></li>
+                                <li><a class="dropdown-item" href="#"><Link to="/productos">Pantalones</Link></a></li>
+                                <li><a class="dropdown-item" href="#"><Link to="/productos">Vestidos</Link></a></li>
+                            </NavDropdown>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#"><Link to="/productos">Ofertas</Link></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#"><Link to="/contacto">Contacto</Link></a>
+                            </li>
+
+
+                        </Nav>
+                        <Nav>
+                            <img class="d-none d-lg-block" src={user}></img>
+                            <NavDropdown title="Clara Visconti" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Mi cuenta</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Carrito</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.4">Cerrar sesión</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="#pricing"><i class="fas fa-search"></i></Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+
+            {/* <div>
                 <nav class="navbar navbar-expand-lg">
 
                     <div class="container-fluid">
@@ -70,7 +108,7 @@ function Router() {
                         </div>
                     </div>
                 </nav>
-            </div>
+            </div> */}
 
 
             <Switch>
