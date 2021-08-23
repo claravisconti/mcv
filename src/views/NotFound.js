e24f87ef8c1880baf2eadd8734b3e10fb6b188c7
Maria Clara Visconti <claravisconti@hotmail.com>
2021-08-23 11:32:18 -0300

    notFound

--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "../App.css";
-import { Container, Row } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+import notFound from "../images/notFound.png";
 
 function NotFound() {
-    return (
-        <div>
-            <Container>
-                <Row>
-
-                    <h1>Not Found</h1>
-        
-                </Row>
-            </Container>
+  return (
+    <div class="container d-flex justify-content-center">
+      <div class="row mb-5">
+        <div class="text-center">
+          <img class="mt-5" src={notFound} alt="Avatar del usuario" />
+          <p class="notFound">Página no encontrada</p>
+          <Link to="/"><button class="btn btn-primary">Volver a Inicio</button></Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
